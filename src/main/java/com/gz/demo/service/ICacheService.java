fa82c0a944499dd3f5e2b9723be1ef957f75bed0
g-zz <gao71676970@vip.qq.com>
2020-03-14 23:53:48 +0800

    1

---
+++ src/main/java/com/gz/demo/service/ICacheService.java
@@ -0,0 +1,8 @@
+package com.gz.demo.service;
+
+/**
+ * Created by gz on 2018/11/1.
+ */
+public interface ICacheService {
+
+}
---
+++ src/main/java/com/gz/demo/service/impl/AbsCacheService.java
@@ -0,0 +1,7 @@
+package com.gz.demo.service.impl;
+
+/**
+ * Created by gz on 2018/11/1.
+ */
+public class AbsCacheService {
+}
